1cf835f69087aebd4f70e0374aef708041f2b2e6
Andreas Baumann <abaumann@yahoo.com>
2013-07-30 17:42:15 +0200

    still component trouble

--- server/database/testdata.sql
+++ server/database/testdata.sql
@@ -12,3 +12,4 @@ INSERT INTO "Project" VALUES(10002,NULL,NULL,NULL,'WOLFCLIENT','Wolfclient',NULL
 INSERT INTO "Project" VALUES(10003,NULL,NULL,NULL,'CONFIGURATOR','Configurator',NULL,'Configurator',3);
 INSERT INTO "Project" VALUES(10004,NULL,NULL,NULL,'CRM','Crm',NULL,'Customer relations management',3);
 INSERT INTO "Project" VALUES(10005,NULL,NULL,NULL,'WOLFZILLA','Wolfzilla',NULL,'Bug reporting',3);
+INSERT INTO "Project" VALUES(10006,NULL,NULL,NULL,'WERP','Werp',NULL,'Simple ERP implementation using Wolframe',3);

1c27110e2736b88cce02636df33f1e052bce750c
Andreas Baumann <abaumann@yahoo.com>
2013-08-12 10:15:02 +0200

    ..

--- phpclient/config.php
+++ phpclient/config.php
@@ -12,5 +12,5 @@ $WOLFRAME_COMBINED_CERTS = "certs/combinedcert.pem";
 
 # which user agents should use client XSLT
 $BROWSERS_USING_CLIENT_XSLT = array(
-	"op12", "ie8", "ie9", "ie10", "moz23", "webkit27", "webkit28"
+	"op12", "ie8", "ie9", "ie10", "moz23", "webkit27", "webkit28", "moz11"
 );

87b52ff70d85f843796da6d1cc8e3b171fa0836b
Andreas Baumann <abaumann@yahoo.com>
2013-09-18 20:36:20 +0100

    relocated web client

--- phpclient/config.php
+++ phpclient/config.php
@@ -4,7 +4,7 @@
 $WOLFRAME_SERVER = "localhost";
  
 # wolframe daemon port
-$WOLFRAME_PORT = 7961;
+$WOLFRAME_PORT = 7962;
 
 # for SSL secured communication, a file with combined client certificate
 # and client key
@@ -12,5 +12,5 @@ $WOLFRAME_COMBINED_CERTS = "certs/combinedcert.pem";
 
 # which user agents should use client XSLT
 $BROWSERS_USING_CLIENT_XSLT = array(
-	"op12", "ie8", "ie9", "ie10", "moz23", "webkit27", "webkit28", "moz11"
+#	"op12", "ie8", "ie9", "ie10", "moz23", "webkit27", "webkit28", "moz11"
 );
